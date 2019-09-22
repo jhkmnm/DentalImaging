15233c0e34317dd4faa03197970bf61e2f1a1752
jhkmnm <jhkmnm_123@163.com>
2019-09-22 16:13:55 +0800

    连接方式选择

---
+++ DentalImaging/新界面/ChannelChoose.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace DentalImaging.新界面
+{
+    public partial class ChannelChoose : Form
+    {
+        public ChannelChoose()
+        {
+            InitializeComponent();
+        }
+    }
+}
